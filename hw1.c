2cf0d60fea8ec316790f622145e5ef975ddefd84
pianonerd7 <pianonerd7@gmail.com>
2015-09-16 22:05:56 -0400

    it compiles for now

--- hw1.c
+++ hw1.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -19,6 +21,60 @@ int print_if_error(int syscall) {
 	else {
 		return syscall;
 	}
+}
+
+time_t time_wrapper() {
+	
+	time_t val = time(NULL);
+	if (val == ((time_t) -1)) {
+		perror("time_wrapper");
+		exit(errno);
+	}
+	else {
+		return val;
+	}	
+}
+
+char* cuserid_wrapper() {
+	char* val = cuserid(NULL);
+	if (val == NULL) {
+		perror("cuserid");
+		exit(errno);
+	}
+	else {
+		return val;
+	}
+}
+
+void getrusage_wrapper(struct rusage* output) {
+	int val = getrusage(RUSAGE_SELF, output);
+	if (val == -1) {
+		perror("getrusage_wrapper");
+		exit(errno);
+	}
+}
+
+void print_exec_times(const char* proc_name) {
+
+	int k = 0;
+	int i;
+
+	//accumulate some CPU time
+	for (i = 0; i < 100000000; i++) {
+		k+=2;
+	}
+	
+	time_t cur_time;
+	struct rusage res_usage;
+
+	cur_time = time_wrapper();
+	getrusage_wrapper(&res_usage);
+
+	printf("[%s] seconds since epoch: %d\n", proc_name, (int)cur_time);
+	printf("[%s] current time is %s", proc_name, ctime(&cur_time));
+	printf("[%s] user CPU time: %d us\n", proc_name, (int)res_usage.ru_utime.tv_usec);
+	printf("[%s] system CPU time: %d us\n", proc_name, (int)res_usage.ru_stime.tv_usec);
+
 }
 
 void check_id_error(int id) {
@@ -58,6 +114,11 @@ int factorial(int n) {
 	return product;
 }
 
+char* get_proc_name(const char* base, char* buff) {
+	sprintf(buff, "%s, PID=%d", base, (int) getpid());
+	return (char*) buff;
+}
+
 int main() {
 
 	pid_t pid;
@@ -65,6 +126,9 @@ int main() {
 	int status;
 	int fork_count;
 	int i;
+
+	char parent_proc_name[255];
+	get_proc_name("parent", parent_proc_name);
 
 	const char * cat_file = "ls";
 	char * const cat_args [] = {"ls -l", NULL};
@@ -85,7 +149,10 @@ int main() {
 	if (pid == 0) { //Child
 
 		pid = getpid();
+		check_id_error(pid);
+
 		ppid = getppid();
+		check_id_error(pid);
 
 		printf("I am child #%d and my pid is %d. My parent is %d. \n", fork_count, pid, ppid);
 
@@ -94,7 +161,7 @@ int main() {
 			pid = getpid();
 			check_id_error(pid);
 
-			printf("%d - (n(n-2)) binomial coefficient computations of integers n=2, 3, 10, start now! \n", pid);
+			printf("The process id %d (fork#%d) announce that (n(n-2)) binomial coefficient computations of integers n=2, 3, 10, start now! \n", pid, fork_count);
 			exit(0);
 		}
 
@@ -123,6 +190,7 @@ int main() {
 				printf("The process id %d (fork#%d) produced the binomial coefficient %d for integer %d. \n", pid, fork_count, binomialCoefficient(i, i-2), i);
 				sleep(4);
 			}
+			sleep(4);
 			exit(0);
 		}
 
@@ -156,6 +224,7 @@ int main() {
 		check_id_error(pid);
 		printf("process %d exited with status %d \n", pid, WEXITSTATUS(status));
 	
+		print_exec_times(parent_proc_name);
 	}
 
 }
