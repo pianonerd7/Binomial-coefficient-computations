b5e02348b4d8f1017deee5f825e1a6d71108fcf1
pianonerd7 <pianonerd7@gmail.com>
2015-09-16 21:31:53 -0400

    changed comments

--- hw1.c
+++ hw1.c
@@ -21,10 +21,10 @@ int print_if_error(int syscall) {
 	}
 }
 
-void check_pid_error(int pid) {
+void check_id_error(int id) {
 
-	if (pid < 0) {
-		perror("pid error");
+	if (id < 0) {
+		perror("id error");
 		exit(errno);
 	}
 }
@@ -61,6 +61,7 @@ int factorial(int n) {
 int main() {
 
 	pid_t pid;
+	pid_t ppid;
 	int status;
 	int fork_count;
 	int i;
@@ -82,20 +83,30 @@ int main() {
 
 
 	if (pid == 0) { //Child
-		printf("I am child #%d and my pid is %d. My parent is %d. \n", fork_count, getpid(), getppid());
+
+		pid = getpid();
+		ppid = getppid();
+
+		printf("I am child #%d and my pid is %d. My parent is %d. \n", fork_count, pid, ppid);
 
 		if (fork_count == 1) {
 
-			printf("%d - (n(n-2)) binomial coefficient computations of integers n=2, 3, 10, start now! \n", getpid());
+			pid = getpid();
+			check_id_error(pid);
+
+			printf("%d - (n(n-2)) binomial coefficient computations of integers n=2, 3, 10, start now! \n", pid);
 			exit(0);
 		}
 
 		else if (fork_count == 2) {
 
 			sleep(3);
+			
+			pid = getpid();
+			check_id_error(pid);
 
 			for (i = 2; i <= 10; i+=2) {
-				printf("%d binomailcoef of %d is %d \n", fork_count, i, binomialCoefficient(i, i-2));
+				printf("The process id %d (fork#%d) produced the binomial coefficient %d for integer %d. \n", pid, fork_count, binomialCoefficient(i, i-2), i);
 				sleep(4);
 			}
 			exit(0);
@@ -105,8 +116,11 @@ int main() {
 
 			sleep(5);
 
+			pid = getpid();
+			check_id_error(pid);
+
 			for (i = 3; i < 10; i+=2) {
-				printf("%d binomailcoef of %d is %d \n", fork_count, i, binomialCoefficient(i, i-2));
+				printf("The process id %d (fork#%d) produced the binomial coefficient %d for integer %d. \n", pid, fork_count, binomialCoefficient(i, i-2), i);
 				sleep(4);
 			}
 			exit(0);
@@ -123,23 +137,23 @@ int main() {
 
 	else { //Parent
 		pid = getpid();
-		check_pid_error(pid);
+		check_id_error(pid);
 		printf("I am the parent, my pid is %d\n.", pid);
 
 		pid = wait(&status);
-		check_pid_error(pid);
+		check_id_error(pid);
 		printf("process %d exited with status %d \n", pid, WEXITSTATUS(status));
 
 		pid = wait(&status);
-		check_pid_error(pid);
+		check_id_error(pid);
 		printf("process %d exited with status %d \n", pid, WEXITSTATUS(status));
 
 		pid = wait(&status);
-		check_pid_error(pid);
+		check_id_error(pid);
 		printf("process %d exited with status %d \n", pid, WEXITSTATUS(status));
 
 		pid = wait(&status);
-		check_pid_error(pid);
+		check_id_error(pid);
 		printf("process %d exited with status %d \n", pid, WEXITSTATUS(status));
 	
 	}
